66048b76183d1abf115176dd8682c2d0a18c45f1
Oleksandr Pavloshchuk <oleksandr.pavloshchuk@openiam.com>
2026-01-05 21:00:03 +0200

    refactor

--- back-app/src/main/java/tutorial/camunda/consumer/loans/tasks/AggregateScoringDelegate.java
+++ back-app/src/main/java/tutorial/camunda/consumer/loans/tasks/AggregateScoringDelegate.java
@@ -13,13 +13,14 @@ import tutorial.camunda.consumer.loans.services.AggregateScoringService;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class AggregateScoringDelegate implements JavaDelegate {
 
-    // TODO (2025/12/27) use configuration data here
+    // TODO (2025/12/27) use configuration data from service here
     private static final int AUTO_REJECT_BELOW = 30;
     private static final int AUTO_APPROVE_ABOVE = 70;
 
@@ -27,43 +28,63 @@ public class AggregateScoringDelegate implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution execution) {
-        final Integer personScores =
-                (Integer) execution.getVariable(VariableNames.personCheckScores.name());
-        final List<String> personReasons =
-                (List<String>) execution.getVariable(VariableNames.personCheckReasons.name());
-        final Integer financeScores =
-                (Integer) execution.getVariable(VariableNames.financeCheckScores.name());
-        final List<String> financeReasons =
-                (List<String>) execution.getVariable(VariableNames.financeCheckReasons.name());
-
         final Map<String, CheckResult> checkResults = Map.of(
-                "personCheck", new CheckResult(personScores, personReasons),
-                "financeCheck", new CheckResult(financeScores, financeReasons)
+                "personCheck", getPersonCheckResult(execution),
+                "financeCheck", getFinanceCheckResult(execution)
         );
         final CheckResult totalCheckResult = aggregateScoringService.aggregate(checkResults);
-
         final int totalScores = totalCheckResult.scores();
-        final List<String> totalReasons = totalCheckResult.reasons();
-
         execution.setVariable(VariableNames.totalScores.name(), totalScores);
-        execution.setVariable(VariableNames.totalReasons.name(), totalReasons);
+        execution.setVariable(VariableNames.totalReasons.name(), totalCheckResult.reasons());
 
-        ScoringResult scoringResult;
-        Decision decision = null;
-        if (totalScores < AUTO_REJECT_BELOW) {
-            scoringResult = ScoringResult.AUTO_REJECT;
-            decision = Decision.REJECT;
-        } else if (totalScores > AUTO_APPROVE_ABOVE) {
-            scoringResult = ScoringResult.AUTO_APPROVE;
-            decision = Decision.APPROVE;
-        } else {
-            scoringResult = ScoringResult.MANUAL;
-        }
+        final ScoringResult scoringResult = getScoringResult(totalScores);
         log.info("scoring result: {}", scoringResult.name());
         execution.setVariable(VariableNames.scoringResult.name(), scoringResult);
-        if (decision != null) {
+        getDecision(totalScores).ifPresent( decision -> {
             log.info("decision: {}", decision.name());
             execution.setVariable(VariableNames.decision.name(), decision);
+        });
+    }
+
+    private static ScoringResult getScoringResult(int scores) {
+        if (scores < AUTO_REJECT_BELOW) {
+            return ScoringResult.AUTO_REJECT;
+        } else if (scores > AUTO_APPROVE_ABOVE) {
+            return ScoringResult.AUTO_APPROVE;
+        } else {
+            return ScoringResult.MANUAL;
         }
+    }
+
+    private static Optional<Decision> getDecision(int scores) {
+        if (scores < AUTO_REJECT_BELOW) {
+            return Optional.of(Decision.REJECT);
+        } else if (scores > AUTO_APPROVE_ABOVE) {
+            return Optional.of(Decision.APPROVE);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private static CheckResult getPersonCheckResult(DelegateExecution execution) {
+        return getCheckResult(execution,
+                VariableNames.personCheckScores,
+                VariableNames.personCheckReasons);
+    }
+
+    private static CheckResult getFinanceCheckResult(DelegateExecution execution) {
+        return getCheckResult(execution,
+                VariableNames.financeCheckScores,
+                VariableNames.financeCheckReasons);
+    }
+
+    private static CheckResult getCheckResult(
+            DelegateExecution execution,
+            VariableNames scoresVar,
+            VariableNames reasonsVar
+    ) {
+        final Integer scores = (Integer) execution.getVariable(scoresVar.name());
+        final List<String> reasons = (List<String>) execution.getVariable(reasonsVar.name());
+        return new CheckResult(scores, reasons);
     }
 }
