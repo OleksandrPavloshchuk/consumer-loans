192144bccbd61d570c035b8dad7a215dc9a47ea5
Oleksandr Pavloshchuk <oleksandr.pavloshchuk@openiam.com>
2026-01-13 20:53:27 +0200

    changes in README.md

--- back-app/scripts/task-list.sh
+++ back-app/scripts/task-list.sh
@@ -6,4 +6,4 @@ curl -v \
   -X GET \
   -u demo:demopass \
   -H "Content-Type: application/json" \
-  http://localhost:9091/engine-rest/task?includeProcessVariables=true
+  https://localhost:9091/engine-rest/task?includeProcessVariables=true
--- front-app/vite.config.ts
+++ front-app/vite.config.ts
@@ -1,17 +1,24 @@
+import fs from "fs";
+import https from "https";
 import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
+
+const agent = new https.Agent({
+    ca: fs.readFileSync("/home/oleksandr/.local/share/mkcert/rootCA.pem", "utf-8"),
+});
 
 export default defineConfig({
-    plugins: [react()],
     server: {
+        https: {
+            key: fs.readFileSync("./certs/localhost-key.pem"),
+            cert: fs.readFileSync("./certs/localhost.pem"),
+        },
         proxy: {
             "/engine-rest": {
-                target: "http://localhost:9091",
-                changeOrigin: true
+                target: "https://localhost:9091",
+                changeOrigin: true,
+                secure: true,
+                agent
             },
         },
     },
-    resolve: {
-        dedupe: ["react", "react-dom"]
-    }
 });
