650263f1bd6820356c7583e2b7ff0dfdb2b42863
Oleksandr Pavloshchuk <oleksandr.pavloshchuk@openiam.com>
2026-01-17 19:27:15 +0200

    changes in test

--- back-app/src/test/java/tutorial/camunda/consumer/loans/auth/service/JwtAuthServiceImplUnitTest.java
+++ back-app/src/test/java/tutorial/camunda/consumer/loans/auth/service/JwtAuthServiceImplUnitTest.java
@@ -1,137 +1,127 @@
 package tutorial.camunda.consumer.loans.auth.service;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import tutorial.auth.jwt.core.dto.LoginRequest;
-import tutorial.auth.jwt.core.dto.LoginResponse;
-import tutorial.auth.jwt.core.dto.RefreshRequest;
-import tutorial.auth.jwt.core.dto.RefreshResponse;
+import tutorial.auth.jwt.core.dto.*;
 import tutorial.auth.jwt.core.service.AuthenticationException;
+import tutorial.auth.jwt.core.service.DateProvider;
+import tutorial.auth.jwt.core.service.JwtProperties;
+import tutorial.auth.jwt.core.service.UserService;
 
+import java.time.Instant;
+import java.util.Date;
 import java.util.Optional;
 import java.util.Set;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 public class JwtAuthServiceImplUnitTest {
 
     @Mock
-    private UserService_DEPRECATED userServiceDEPRECATED;
+    private UserService userService;
+
     @Mock
-    private JwtProviderService jwtProviderService;
+    private JwtProperties jwtProperties;
+
+    @Mock
+    private DateProvider dateProvider;
 
     @InjectMocks
     private JwtAuthServiceImpl jwtAuthServiceImpl;
 
     @Test
     public void login_OK() throws AuthenticationException {
+
+        doReturn("rH6fM4+J4v7QhJkQ2M1hA+PZpZC9xT2l8Y8kXJ6v1aE=")
+                .when(jwtProperties).getSecret();
+        doReturn(100L)
+                .when(jwtProperties).getAccessMinutes();
+        doReturn(11L)
+                .when(jwtProperties).getRefreshDays();
+
+        doReturn(getDateFrom("2026-01-26T00:00:00Z"))
+                .when(dateProvider).createdAt();
+
         doReturn(
-                Optional.of(new User("user-1", "password-2",
-                        Set.of(new SimpleGrantedAuthority("user-3"))
+                Optional.of(new BaseUserInfo("user-1", "password-2",
+                        Set.of("user-3")
                 )))
-                .when(userServiceDEPRECATED)
-                .getUserDetails(any());
-        doReturn("AT-0").when(jwtProviderService).createAccessToken(any());
-        doReturn("RT-1").when(jwtProviderService).createRefreshToken(any());
+                .when(userService)
+                .getUser(any());
         final LoginResponse loginResponse = jwtAuthServiceImpl.login(
                 new LoginRequest("user-1", "password-2")
         );
-        Assertions.assertEquals("AT-0", loginResponse.accessToken());
-        Assertions.assertEquals("RT-1", loginResponse.refreshToken());
-        ArgumentCaptor<UserDetails> captor = ArgumentCaptor.forClass(UserDetails.class);
-        verify(jwtProviderService).createAccessToken(captor.capture());
-        verify(jwtProviderService).createRefreshToken(captor.capture());
-        UserDetails userDetails = captor.getAllValues().get(0);
-        Assertions.assertEquals("user-1", userDetails.getUsername());
-        Assertions.assertEquals("password-2", userDetails.getPassword());
-        Assertions.assertEquals(Set.of(new SimpleGrantedAuthority("user-3")), userDetails.getAuthorities());
-        userDetails = captor.getAllValues().get(1);
-        Assertions.assertEquals("user-1", userDetails.getUsername());
-        Assertions.assertEquals("password-2", userDetails.getPassword());
-        Assertions.assertEquals(Set.of(new SimpleGrantedAuthority("user-3")), userDetails.getAuthorities());
+        Assertions.assertEquals(
+                "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyLTEiLCJyb2xlcyI6WyJ1c2VyLTMiXSwiaWF0IjoxNzY5Mzg1NjAwfQ.U7JRR7y4iuNOwLEj3hBrEp0rao5TtQhqObcdPw90r1s",
+                loginResponse.accessToken());
+        Assertions.assertEquals(
+                "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyLTEiLCJyb2xlcyI6WyJ1c2VyLTMiXSwiaWF0IjoxNzY5Mzg1NjAwfQ.U7JRR7y4iuNOwLEj3hBrEp0rao5TtQhqObcdPw90r1s",
+                loginResponse.refreshToken());
     }
 
     @Test
     public void login_BadPassword() {
         doReturn(
-                Optional.of(new User("user-1", "password-2",
-                        Set.of(new SimpleGrantedAuthority("user-3"))
+                Optional.of(new BaseUserInfo("user-1", "password-2",
+                        Set.of("user-3")
                 )))
-                .when(userServiceDEPRECATED)
-                .getUserDetails(any());
+                .when(userService)
+                .getUser(any());
         Assertions.assertThrows( BadCredentialsException.class,
                 () -> jwtAuthServiceImpl.login(
                         new LoginRequest("user-1", "bebebe")
                 )
         );
-        verify(jwtProviderService, never()).createAccessToken(any());
-        verify(jwtProviderService, never()).createRefreshToken(any());
     }
 
     @Test
     public void login_NoUser() {
         doReturn(Optional.empty())
-                .when(userServiceDEPRECATED)
-                .getUserDetails(any());
+                .when(userService)
+                .getUser(any());
         Assertions.assertThrows( BadCredentialsException.class,
                 () -> jwtAuthServiceImpl.login(
                         new LoginRequest("user-5", "bububu")
                 )
         );
-        verify(jwtProviderService, never()).createAccessToken(any());
-        verify(jwtProviderService, never()).createRefreshToken(any());
     }
 
     @Test
     public void refresh_OK() throws AuthenticationException {
-        final Claims claims = Jwts.claims().setSubject("user-1");
-        doReturn(claims).when(jwtProviderService).parseRefreshToken(any());
-        doReturn(
-                Optional.of(new User("user-1", "password-2",
-                        Set.of(new SimpleGrantedAuthority("user-3"))
-                )))
-                .when(userServiceDEPRECATED)
-                .getUserDetails(any());
-        doReturn("AT-A").when(jwtProviderService).createAccessToken(any());
-        final RefreshResponse refreshResponse = jwtAuthServiceImpl.refresh(
-                new RefreshRequest("rt")
-        );
-        Assertions.assertEquals("AT-A", refreshResponse.accessToken());
-        ArgumentCaptor<UserDetails> captor = ArgumentCaptor.forClass(UserDetails.class);
-        verify(jwtProviderService).createAccessToken(captor.capture());
-        verify(jwtProviderService, never()).createRefreshToken(captor.capture());
-        UserDetails userDetails = captor.getAllValues().get(0);
-        Assertions.assertEquals("user-1", userDetails.getUsername());
-        Assertions.assertEquals("password-2", userDetails.getPassword());
-        Assertions.assertEquals(Set.of(new SimpleGrantedAuthority("user-3")), userDetails.getAuthorities());
-    }
+        doReturn("rH6fM4+J4v7QhJkQ2M1hA+PZpZC9xT2l8Y8kXJ6v1aE=")
+                .when(jwtProperties).getSecret();
+        doReturn(100L)
+                .when(jwtProperties).getAccessMinutes();
 
-    @Test
-    public void refresh_NoUser() {
-        final Claims claims = Jwts.claims().setSubject("user-1");
-        doReturn(claims).when(jwtProviderService).parseRefreshToken(any());
-        doReturn(Optional.empty())
-                .when(userServiceDEPRECATED)
-                .getUserDetails(any());
-        Assertions.assertThrows( BadCredentialsException.class,
-                () -> jwtAuthServiceImpl.refresh(
-                        new RefreshRequest("token")
+        doReturn(getDateFrom("2026-01-26T00:00:00Z"))
+                .when(dateProvider).createdAt();
+        doReturn(getDateFrom("2026-01-26T01:00:00Z"))
+                .when(dateProvider).checkedAt();
+
+        doReturn(
+                Optional.of(new BaseUserInfo("user-1", "password-2",
+                        Set.of("user-3")
+                )))
+                .when(userService)
+                .getUser(any());
+        final RefreshResponse refreshResponse = jwtAuthServiceImpl.refresh(
+                new RefreshRequest(
+                        "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyLTEiLCJyb2xlcyI6WyJ1c2VyLTMiXSwiaWF0IjoxNzY5Mzg1NjAwfQ.U7JRR7y4iuNOwLEj3hBrEp0rao5TtQhqObcdPw90r1s"
                 )
         );
-        verify(jwtProviderService, never()).createAccessToken(any());
-        verify(jwtProviderService, never()).createRefreshToken(any());
+        Assertions.assertEquals(
+                "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyLTEiLCJyb2xlcyI6WyJ1c2VyLTMiXSwiaWF0IjoxNzY5Mzg1NjAwfQ.U7JRR7y4iuNOwLEj3hBrEp0rao5TtQhqObcdPw90r1s",
+                refreshResponse.accessToken());
+    }
+
+    private static Date getDateFrom(String src) {
+        return Date.from(Instant.parse(src));
     }
 }
